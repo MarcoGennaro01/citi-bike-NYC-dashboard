7a6f9e7865c2cf23129b01c879159ef423f9ae72
Marco Linux <marcog01@outlook.it>
2025-11-03 21:38:53 +0100

    added plot

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import numpy as np
 
 rides = pd.read_parquet("/home/mark/git/bike-data-newyork/data_parquet/rides.parq")
+
 # Computing average duration distribution
 rides = rides[rides["duration"] < 120]
 avg_duration = px.histogram(
@@ -15,16 +15,30 @@ avg_duration = px.histogram(
     facet_row="is_electric",
     nbins=40,
 )
+
 # Computing rides distribution through months and hours
-rides_reduced = (
-    rides.groupby("starting_date_hour")
-    .count()
-    .loc["starting_date_hour", "start_station_id"]
-)
+rides_reduced = rides.groupby("starting_date_hour").count().reset_index(drop=False)
 rides_reduced["month"] = rides_reduced["starting_date_hour"].apply(lambda x: x.month)
 rides_reduced["hour"] = rides_reduced["starting_date_hour"].apply(lambda x: x.hour)
 rides_reduced = rides_reduced.loc[:, ["month", "hour", "is_electric"]]
 rides_reduced = rides_reduced.rename(columns={"is_electric": "count"})
-mean_hour = rides_reduced.groupby("hour").mean().reset_index(drop=True).loc[:, "count"]
 
-mean_hour_bar = px.bar(mean_hour, x=mean_hour.index, y="count")
+# Plotting average hourly rides
+mean_hour = (
+    rides_reduced.groupby("hour")
+    .mean()
+    .reset_index(drop=False)
+    .rename(columns={"count": "mean"})
+    .loc[:, ["mean", "hour"]]
+)
+mean_hour_bar = px.bar(mean_hour, x="hour", y="mean")
+
+# Plotting average hourly rides per month
+mean_month = (
+    rides_reduced.groupby("month")
+    .mean()
+    .reset_index(drop=False)
+    .rename(columns={"count": "mean"})
+    .loc[:, ["mean", "month"]]
+)
+mean_month_bar = px.bar(mean_month, x="month", y="mean")
