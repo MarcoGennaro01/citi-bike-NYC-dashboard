80dca460ce40607cfb99ce18d16eb4d85d5173c2
Marco Linux <marcog01@outlook.it>
2025-11-06 19:19:21 +0100

    added .py files

---
+++ pie.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from geopy.distance import geodesic
+
+rides = pd.read_parquet("/home/mark/git/bike-data-newyork/data_parquet/rides.parq")
+stations = pd.read_parquet("./data_parquet/stations.parq")
+
+
+member_pie = px.pie(
+    rides.groupby("is_m")
+    .size()
+    .reset_index(name="count")
+    .loc[:, ["is_m", "count"]]
+    .replace({False: "Not Member", True: "Member"}),
+    color="is_m",
+    values="count",
+    hover_name="is_m",
+    color_discrete_map={
+        "Member": "hsv(241.58, 77.55%, 19.22%)",
+        "Not Member": "hsv(197.18, 100%, 86.27%)",
+    },
+)
+
+electric_pie = px.pie(
+    rides.groupby("is_electric")
+    .size()
+    .reset_index(name="count")
+    .loc[:, ["is_electric", "count"]]
+    .replace({False: "Not Electric", True: "Electric"}),
+    color="is_electric",
+    values="count",
+    hover_name="is_electric",
+    color_discrete_map={
+        "Electric": "hsv(11.53, 84.58%, 94.12%)",
+        "Not Electric": "hsv(63, 18.43%, 85.1%)",
+    },
+)
