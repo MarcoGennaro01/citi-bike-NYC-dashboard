870127276dba8d19f43e68e95fc87e18f9d17f39
Marco Linux <marcog01@outlook.it>
2025-11-11 23:26:43 +0100

    main.py

---
+++ weather.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from meteostat import Hourly
+
+start = datetime(2024, 1, 1)
+end = datetime(2024, 12, 31, 23, 59)
+weather_data = Hourly("KJRB0", start, end)
+weather_data = weather_data.fetch()
+weather_data["hour"] = [x.hour for x in weather_data.index]
+weather_data.to_parquet("./cleaned_data/weather.parquet")
